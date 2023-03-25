88dc859bb2c10b164dd1496e7dcf8d4198077814
vinnie post <vinnie.post1@gmail.com>
2023-03-25 23:35:43 +0100

    added plot to main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import requests
 import json
-
+from matplotlib import pyplot
+from datetime import datetime, timedelta
 # Er kunnen meer plaatsen worden aangemaakt
 
 
 class Antwerpen:
-    latitude = 51.22
-    longitude = 4.40
-    hourly = "temperature_2m"
+    def __init__(self):
+        self.latitude = 51.22
+        self.longitude = 4.40
+        self.hourly = "temperature_2m"
 
 
 class Data:
@@ -15,6 +17,7 @@ class Data:
         self.DataLokatie = "Data/data.json"
         with open(self.DataLokatie, "r") as file:
             self.data = json.loads(file.read())
+        self.currentDate = datetime.now().strftime("%Y-%m-%d")
 
     def LoadData(self):
         with open(self.DataLokatie, "r") as file:
@@ -27,7 +30,8 @@ def Init():
 
 
 def ContactServer():
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={Antwerpen.latitude}&longitude={Antwerpen.longitude}&hourly={Antwerpen.hourly}"
+    lokatie = Antwerpen()
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lokatie.latitude}&longitude={lokatie.longitude}&hourly={Antwerpen.hourly}"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -49,8 +53,6 @@ def TemperatuurOpTijd():
     print(f"de temperatuur op {timeIndex} uur is {temperature} C")
 
 
-# input kan meer user friendly gemaakt worden
-
 def TempOpUur():
     currentData = Data()
     datum = input("Geef een datum op in formaat (2023-03-25): ")
@@ -64,8 +66,33 @@ def TempOpUur():
     print(f"de temperatuur op {timeIndex} uur is {temperature} C")
 
 
+def Plot():
+    currentData = Data()
+    hourly = currentData.data["hourly"]
+    times = hourly["time"]
+    currentDayTimes = []
+    currentDayTemperatures = []
+    for i, time in enumerate(times):
+        if time.startswith(currentData.currentDate):
+            currentDayTimes.append(
+                datetime.fromisoformat(time).strftime("%H:%M"))
+            currentDayTemperatures.append(hourly["temperature_2m"][i])
+    temperature_2m = hourly["temperature_2m"]
+    pyplot.plot(currentDayTimes, currentDayTemperatures,
+                label="Temperatuur", color="red", marker="o", linestyle="dashed")
+    pyplot.xticks(range(0, len(currentDayTimes), 1), rotation=45)
+    for time, temperature in zip(currentDayTimes, currentDayTemperatures):
+        pyplot.annotate(f"{temperature} C",
+                        (time, temperature+0.2), ha="center", rotation=45)
+
+    pyplot.xlabel("Tijd")
+    pyplot.ylabel("Temperatuur")
+    pyplot.title("Temperatuur in Antwerpen op " + currentData.currentDate)
+    pyplot.show()
+
+
 def Main():
-    TempOpUur()
+    Plot()
 
 
 if __name__ == "__main__":
